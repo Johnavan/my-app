a8511fd5e09aab6ee9a618a3838c404cda70683e
Johnavan Thomas <johnavanthomas@gmail.com>
2019-08-28 22:33:10 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p class= "summary">
+        Hello, my name is Johnavan Thomas.
+        Currently in my 4th year of Computer Science at Carleton University.
+        When I'm not in class, I'm a Product Designer at <a href="https://www.fullscript.com">Fullscript</a>. 
+        </p>
+
+        <p class="links">
+            <a class="social" href="mailto:johnavanthomas@gmail.com">Email</a>
+            <a class="social" href="https://github.com/Johnavan">Github</a>
+            <a class="social" target="_blank" href="https://www.instagram.com/jvannthomas/">Instagram</a>
+            <a class="social" target="_blank" href="https://www.linkedin.com/in/johnavan-thomas-017b6a112/">Linkedin</a>
+          </p>
+    
+
+        
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
